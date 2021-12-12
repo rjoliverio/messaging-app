83abc836f984ba933f17a91b91350f24cabafa7a
Rj Oliverio <rjoliverio18@gmail.com>
2021-12-12 08:43:34 +0800

    bootstrap init

--- socket-react-app/src/App.js
+++ socket-react-app/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/js/bootstrap.min.js";
+import { Routes, Route} from "react-router-dom";
+import HomeComponent from './routes/index.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className='text-white mt-5'>Omingle</h1>
+      <Routes>
+        <Route path="/" element={<HomeComponent />} />
+      </Routes>
     </div>
   );
 }
---
+++ socket-react-app/src/routes/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeComponent = () => {
+    return (
+        <div className='text-white'>
+            Hello world
+        </div>
+    )
+}
+
+export default HomeComponent
