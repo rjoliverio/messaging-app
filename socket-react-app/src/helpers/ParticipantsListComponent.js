5e3c36456b04dbaea9ba8f054246d2770c024427
aljannondoy <54362999+aljannondoy@users.noreply.github.com>
2021-12-19 11:35:25 +0800

    Update socket dynamic participants

--- socket-react-app/src/helpers/ParticipantsListComponent.js
+++ socket-react-app/src/helpers/ParticipantsListComponent.js
@@ -8,11 +8,16 @@ const ParticipantsListComponent = (props) => {
   const[inactive,setInactive]=useState([]);
   useEffect(() => {
     socket.on("list", (data) => {
-      setJoin([...data.user]);
+      setJoin(data.user);
       if(data.deletedUsers) setInactive(data.deletedUsers);
     });
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+  function objExist(arr, user, group) {
+    return arr.some(function(el) {
+      return el.user === user && el.group === group;
+    }); 
+  }
   
   console.log("PARTI",join, inactive);
   return (
@@ -20,16 +25,17 @@ const ParticipantsListComponent = (props) => {
             <h5 className="text-white"><i className="fa fa-user text-light" aria-hidden="true"></i> Group Participants</h5>
             <ul>
             {join.map((user,i) => (
-                   <p key={i}> <span className="dotOnline"></span> <span className="text-white align-left" >{user}</span></p>
+            (user.group === props.group)?
+                   <p key={i}> <span className="dotOnline"></span> <span className="text-white align-left" >{user.user}</span></p>:""
                   
             ))}
             {inactive.map((inactive,i)=> (
-              (props.participants.includes(inactive) || inactive === "" || join.includes(inactive))? "":
-                    <p key={i}> <span className="dot"></span> <span className="text-white align-left" >{inactive}</span></p>
+              (inactive.group === props.group && objExist(join, inactive.user, inactive.group) == false)? <p key={i}> <span className="dot"></span> <span className="text-white align-left" >{inactive.user}</span></p>:""
+                    
               ))}
                   {props.participants.map((offline,i)=> (
-                    (join.includes(offline) || (join.includes(offline) && inactive.includes(offline)) )? "":
-                    <p key={i}> <span className="dot"></span> <span className="text-white align-left" >{offline}</span></p>
+                    (offline.group === props.group && objExist(join, offline.user, offline.group) == false && objExist(inactive, offline.user, offline.group) == false)? <p key={i}> <span className="dot"></span> <span className="text-white align-left" >{offline.user}</span></p>:""
+                    
                   ))}
             </ul>
         </Row>
